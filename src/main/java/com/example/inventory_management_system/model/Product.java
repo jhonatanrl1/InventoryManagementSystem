94ba0503471381bc4fc2f95de026571ba4af1783
Jhonatan Ramirez <jhonatanramirezlagunas@gmail.com>
2026-09-06 14:44:03 -0500

    Add Product primary key mapping

--- src/main/java/com/example/inventory_management_system/model/Product.java
+++ src/main/java/com/example/inventory_management_system/model/Product.java
@@ -1,6 +1,9 @@
 package com.example.inventory_management_system.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 
 
@@ -8,7 +11,10 @@ import java.math.BigDecimal;
 public class Product {
 
     //Product entity Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
+
     private String name;
     private String description;
     private BigDecimal sellingPrice;
