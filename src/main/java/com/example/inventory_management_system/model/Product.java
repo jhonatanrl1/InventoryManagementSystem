a6003792cf531646bbad9023bc1ec83a31646cbc
Jhonatan Ramirez <jhonatanramirezlagunas@gmail.com>
2026-09-06 14:17:20 -0500

    Add Product entity fields

--- src/main/java/com/example/inventory_management_system/model/Product.java
+++ src/main/java/com/example/inventory_management_system/model/Product.java
@@ -1,8 +1,24 @@
 package com.example.inventory_management_system.model;
 
 import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+
 
 @Entity
 public class Product {
+
+    //Product entity Fields
+    private Long productId;
+    private String name;
+    private String description;
+    private BigDecimal sellingPrice;
+    private int quantityInStock;
+    private int lowStockThreshold;
+
+
+
+
+
+
 
 }
