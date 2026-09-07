e8042223b58497bea108b9cacb04414af98d2449
Jhonatan Ramirez <jhonatanramirezlagunas@gmail.com>
2026-09-07 13:58:25 -0500

    Connect ProductService to ProductRepository

--- src/main/java/com/example/inventory_management_system/service/ProductService.java
+++ src/main/java/com/example/inventory_management_system/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.example.inventory_management_system.service;
 
+
+import com.example.inventory_management_system.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
+
